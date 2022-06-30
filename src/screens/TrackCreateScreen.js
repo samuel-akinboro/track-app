619e993092e469789bfffe13e1bd62e1e1af5ce8
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-30 20:03:34 +0100

    watch location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,19 +1,25 @@
+import '../_mockLocation'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React, { useState, useEffect } from 'react';
 import Map from '../components/Map'
-import * as Location from 'expo-location'
-import '../_mockLocation'
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'
 
 const TrackCreateScreen = () => {
   const [errorMessage, setErrorMessage] = useState(null);
 
   const startTracking = async () => {
     try {
-      const { status } = Location.requestForegroundPermissionsAsync();
+      const { status } = requestForegroundPermissionsAsync();
       if(status !== 'granted'){
         console.log('permission not granted');
-        return;
       }
+      watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      }, (location) => {
+        // console.log(location)
+      });
     } catch (err) {
       setErrorMessage(err.message)
     }
