07be49ce2b6b5c8de31883654c25edd892fb4baa
victor-akinboro <victorakinboro2003@gmail.com>
2023-01-14 15:19:35 +0700

    cleaning up buggy useEffect in useLocation

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,35 +3,41 @@ import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 
 
 export default (shouldTrack, callback) => {
   const [errorMessage, setErrorMessage] = useState(null);
-  const [subscriber, setSubscriber] = useState(null)
-
-  const startTracking = async () => {
-    try {
-      const { status } = await requestForegroundPermissionsAsync();
-      console.log('status', status)
-      if(status !== 'granted'){
-        console.log('permission not granted');
-      }
-      const sub = await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 10
-      }, callback
-      );
-      setSubscriber(sub)
-    } catch (err) {
-      setErrorMessage(err.message)
-    }
-  }
 
   useEffect(()=> {
+    let subscriber;
+    const startTracking = async () => {
+      try {
+        const { status } = await requestForegroundPermissionsAsync();
+        if(status !== 'granted'){
+          console.log('permission not granted');
+        }
+        subscriber = await watchPositionAsync({
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        }, callback
+        );
+      } catch (err) {
+        setErrorMessage(err.message);
+      }
+    }
+
     if(shouldTrack){
       startTracking();
     }else{
-      subscriber.remove()
-      setSubscriber(null)
+      if(subscriber) {
+        subscriber.remove();
+        subscriber = null;
+      }
     }
-  }, [shouldTrack]);
+
+    return () => {
+      if(subscriber){
+        subscriber.remove()
+      }
+    }
+  }, [shouldTrack, callback]);
 
   return [errorMessage]
 }
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,6 +1,6 @@
-// import '../_mockLocation'
+import '../_mockLocation'
 import { StyleSheet, Text, SafeAreaView } from 'react-native'
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import Map from '../components/Map'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation';
@@ -9,7 +9,8 @@ import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
   const { state, addLocation } = useContext(LocationContext);
-  const [errorMessage] = useLocation(isFocused, (location) => addLocation(location, state.recording))
+  const callback = useCallback((location) => addLocation(location, state.recording), [state.recording]);
+  const [errorMessage] = useLocation(isFocused, callback)
 
   return (
     <SafeAreaView>
